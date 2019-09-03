659b036fb78c988fa9f025ff8b973ff6a8940ed8
EC2 Default User <ec2-user@ip-172-31-22-67.ap-southeast-1.compute.internal>
2019-09-03 01:31:42 +0000

    Finish basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
+  get 'sessions/new'
 
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   root 'static_pages#home'
-  
+  resources :users
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
